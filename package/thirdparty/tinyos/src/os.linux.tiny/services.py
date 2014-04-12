67581fb40701e56c5841ab84913f93ff0018c962
Josh <josh@josh-linux.(none)>
2014-04-12 15:06:55 +1200

    [ITV05] Updated init plugins

--- package/thirdparty/tinyos/src/os.linux.tiny/services.py
+++ package/thirdparty/tinyos/src/os.linux.tiny/services.py
@@ -7,11 +7,3 @@ from services.firstrun import firstrun
 firstrun_thread = firstrun()     
 firstrun_thread.onStart()
 
-from services.upgrade import upgrade
-upgrade_thread = upgrade()     
-upgrade_thread.onStart()
-
-from services.resize import resize
-resize_thread = resize()     
-resize_thread.onStart()
-
